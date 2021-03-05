9086baeef560f829e32f7664f98e644ae59810e0
Ankita Puri <54734002+ankitapuri@users.noreply.github.com>
2021-03-05 10:05:50 +0530

    Create day3.cpp

---
+++ March/day3.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       int i = 0;
+        sort(nums.begin(),nums.end());
+        for(i=0;i<nums.size();i++)
+        {  if(nums[i]!=i)
+               return i ;
+        }
+         return i;
+    }
+};
